e13c2c04338f0a0127b139c53f178f12cd4f86e7
rstahl25 <rstahl2@walsh.edu>
2023-10-08 17:44:50 -0400

    Edit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to my first react application!
+          Welcome to my First React application!
         </p>
         <a
           className="App-link"
